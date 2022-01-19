d842c352ad72ff7102f98aa53dfd3fb90b0e4eec
Антон Шмидт <schmidt.ajax@gmail.com>
2022-01-19 21:00:25 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -6,10 +6,31 @@
 using namespace std;
 
 class Solution {
+  void generateInternal(string &s, int opened, int closed, int max_opened, vector<string> &result) {
+    if (s.length() == max_opened * 2) {
+      result.push_back(s);
+      return;
+    }
+
+    if (opened < max_opened) {
+      s.push_back('(');
+      generateInternal(s, opened + 1, closed, max_opened, result);
+      s.pop_back();
+    }
+    if (closed < opened) {
+      s.push_back(')');
+      generateInternal(s, opened, closed + 1, max_opened, result);
+      s.pop_back();
+    }
+  }
  public:
   vector<string> generateParenthesis(int n) {
-    // FIXME
-    return {};
+    string s{"("s};
+    int opened = 1;
+    vector<string> result;
+    generateInternal(s, opened, 0, n, result);
+
+    return result;
   }
 };
 
